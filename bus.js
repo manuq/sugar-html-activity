2d9edfac894d00b4c2ee74ac2eb5f32369b1a3e4
Daniel Narvaez <dwnarvaez@gmail.com>
2013-05-10 20:47:06 +0200

    Get port/key from the window properties
    This is cleaner and will allow the python activity to
    set them up for every pages, not just the first loaded one.

--- bus.js
+++ bus.js
@@ -5,21 +5,12 @@ define(function(require) {
     var socket = null;
 
     function start() {
-        var params = {};
-
-        var search = window.location.search.substring(1);
-        var splitted_search = search.split("&");
-        for (var i = 0; i < splitted_search.length; i++) {
-            var splitted_param = splitted_search[i].split("=");
-            params[splitted_param[0]] = splitted_param[1];
-        }
-
-        socket = new WebSocket("ws://localhost:" + params.port);
+        socket = new WebSocket("ws://localhost:" + window.sugarPort);
 
         socket.onopen = function() {
             socket.send(JSON.stringify({"method": "authenticate",
                                         "id": "authenticate",
-                                        "params": [params.key]}));
+                                        "params": [window.sugarKey]}));
 
             while (messageQueue.length > 0) {
                 socket.send(messageQueue.pop());
@@ -36,13 +27,17 @@ define(function(require) {
         };
     }
 
+    if (window.sugarKey) {
+        start();
+    } else {
+        window.onSugarKeySet = function() {
+            start();
+        }
+    }
+
     var Bus = {};
 
     Bus.sendMessage = function(method, params, callback) {
-        if (socket === null) {
-            start();
-        }
-
         message = {"method": method,
                    "id": lastId,
                    "params": params};
