ce074dec48f1392b6cdfe7120c9076af0cc13014
Daniel Narvaez <dwnarvaez@gmail.com>
2013-05-12 12:26:39 +0200

    Add InputStream support
    This is going to be used by datastore.

--- bus.js
+++ bus.js
@@ -3,9 +3,11 @@ define(function(require) {
     var callbacks = {};
     var queue = [];
     var socket = null;
+    var inputStreams = [];
 
     function start() {
         socket = new WebSocket("ws://localhost:" + window.sugarPort);
+        socket.binaryType = "arraybuffer"
 
         socket.onopen = function() {
             params = [window.sugarId, window.sugarKey]
@@ -20,6 +22,18 @@ define(function(require) {
         };
 
         socket.onmessage = function(message) {
+            if (typeof message.data != "string") {
+                var dataView = new Uint8Array(message.data);
+                var streamId = dataView[0];
+
+                if (streamId in inputStreams) {
+                    var inputStream = inputStreams[streamId];
+                    inputStream.gotData(message.data.slice(1));
+                }
+
+                return;
+            }
+
             var parsed = JSON.parse(message.data);
             var responseId = parsed.id;
             if (responseId in callbacks) {
@@ -49,6 +63,50 @@ define(function(require) {
 
     var Bus = {};
 
+    function InputStream() {
+        this.streamId = null;
+        this._readCallback = null;
+    }
+
+    InputStream.prototype.open = function(callback) {
+        var me = this;
+        Bus.sendMessage("open_stream", [], function(result) {
+            me.streamId = result;
+            inputStreams[me.streamId] = me;
+            callback();
+        });
+    };
+
+    InputStream.prototype.read = function(count, callback) {
+        if (this._readCallback) {
+            throw Error("Read already in progress");
+        }
+
+        this._readCallback = callback;
+
+        var buffer = new ArrayBuffer(8);
+
+        var headerView = new Uint8Array(buffer, 0, 1);
+        headerView[0] = this.streamId;
+
+        var bodyView = new Uint32Array(buffer, 4, 1);
+        bodyView[0] = count;
+
+        Bus.sendBinary(buffer);
+    };
+
+    InputStream.prototype.gotData = function(buffer) {
+        this._readCallback(buffer);
+        this._readCallback = null;
+    }
+
+    InputStream.prototype.close = function() {
+        var me = this;
+        Bus.sendMessage("close_stream", [this.streamId], function() {
+            delete inputStreams[me.streamId];
+        });
+    };
+
     function OutputStream() {
         this.streamId = null;
     };
@@ -73,6 +131,10 @@ define(function(require) {
 
     OutputStream.prototype.close = function() {
         Bus.sendMessage("close_stream", [this.streamId]);
+    };
+
+    Bus.createInputStream = function(callback) {
+        return new InputStream();
     };
 
     Bus.createOutputStream = function(callback) {
